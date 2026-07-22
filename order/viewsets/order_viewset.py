987587041d2a82c7960de1eedbd82fa4476466db
lucas-mcd <16lucascm@gmail.com>
2026-07-22 16:24:16 -0300

    feat: add token authentication to order viewset

--- order/viewsets/order_viewset.py
+++ order/viewsets/order_viewset.py
@@ -1,11 +1,17 @@
-from rest_framework import status
-from rest_framework.mixins import CreateModelMixin
-from rest_framework.response import Response
+from rest_framework.authentication import TokenAuthentication
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ModelViewSet
 
 from order.models import Order
 from order.serializers import OrderSerializer
 
+
 class OrderViewSet(ModelViewSet):
+    authentication_classes = [TokenAuthentication]
+    permission_classes = [IsAuthenticated]
+
     serializer_class = OrderSerializer
     queryset = Order.objects.all()
+
+    def get_queryset(self):
+        return Order.objects.filter(user=self.request.user)
--- product/tests/test_viewsets/test_product_viewset.py
+++ product/tests/test_viewsets/test_product_viewset.py
@@ -36,4 +36,4 @@ class ProductViewSetTest(APITestCase):
         response = self.client.get("/bookstore/v1/product/")
 
         self.assertEqual(response.status_code, status.HTTP_200_OK)
-        self.assertEqual(len(response.data), 2)
+        self.assertEqual(len(response.data["results"]), 2)
